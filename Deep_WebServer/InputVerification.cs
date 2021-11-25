969979afb425676d5c16af3706e33d285b5d14e3
mccarski <48197727+mccarski@users.noreply.github.com>
2021-11-24 21:01:08 -0500

    verification logic

--- Deep_WebServer/InputVerification.cs
+++ Deep_WebServer/InputVerification.cs
@@ -10,24 +10,53 @@ namespace WDD_A6
 {
     class InputVerification
     {
-        // validate web root
+        private static string SearchTextForRegExp(string text, string regExp)
+        {
+            Regex myRegex = new Regex(regExp);
+            var match = myRegex.Match(text);
+            return match.Groups[1].Value;
+        }
+
+        // ******* web root *******
+        // get web root directory
+
+        // verify that web root directory exists
         public static bool ValidateWebRoot(string webRoot)
         {
             return Directory.Exists(webRoot);
         }
 
-        public static bool ValidateEntireWebRoot(string text)
+        // verify the web root
+        public static bool ValidateEntireWebRoot(string text, out string updateWebRoot)
         {
             string regExpRoot = @"^–webRoot=(.*)$";
-            Regex myRegex = new Regex(regExpRoot);
-            return myRegex.IsMatch(text);
+            string webRoot = SearchTextForRegExp(text, regExpRoot);
+            updateWebRoot = webRoot;
+            return ValidateWebRoot(webRoot);
         }
 
+
+
+        // ******* web ip *******
+        // verify the host name
         public static bool ValidateWebIp(string host)
         {
             return System.Uri.CheckHostName(host) != UriHostNameType.Unknown;
         }
 
+        // get web ip directory
+        public static bool ValidateEntireWebIp(string text, out string updateWebIp)
+        {
+            string regExpRoot = @"^–webIP=(.*)$";
+            string webIp = SearchTextForRegExp(text, regExpRoot);
+            updateWebIp = webIp;
+            return ValidateWebIp(webIp);
+        }
+
+
+
+        // ******* server port *******
+        // validate server port
         public static bool ValidateServerPort(string port)
         {
             bool result = int.TryParse(port, out int intPort);
@@ -36,6 +65,15 @@ namespace WDD_A6
                 return true;
             }
             return false;
+        }
+
+        // verify the server port
+        public static bool ValidateEntireServerPort(string text, out string updateServerPort)
+        {
+            string regExpRoot = @"^–webPort=(.*)$";
+            string serverPort = SearchTextForRegExp(text, regExpRoot);
+            updateServerPort = serverPort;
+            return ValidateServerPort(serverPort);
         }
     }
 }
