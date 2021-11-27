4a0eb726253e78001249705c830dc8b2c828f5e0
mccarski <48197727+mccarski@users.noreply.github.com>
2021-11-26 21:15:17 -0500

    error types

---
+++ Deep_WebServer/Error/ErrorType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myOwnWebServer.Error
+{
+    static class ErrorType
+    {
+        enum ErrorTypes : short
+        {
+            CONTINUE = 100,
+            OK = 200,
+            MOVED_PERMANEMTNLY = 301,
+            NOT_FOUND = 404,
+            SERVER_ERROR = 500
+        }
+    }
+}
