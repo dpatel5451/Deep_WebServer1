22a36920f43659a24e42c3960a802341affbe031
mccarski <48197727+mccarski@users.noreply.github.com>
2021-11-27 22:32:00 -0500

    client request comments

--- Deep_WebServer/ClientRequest.cs
+++ Deep_WebServer/ClientRequest.cs
@@ -3,52 +3,79 @@
 * Project		:	PROG2001 - Assignment 06 
 * Programmer	:	Deep Patel & Sean Mccarthy
 * First Version	:	24/11/2021
-* Description	:	It will verify that user input matches the requirements. 
-*                   It will verify user entered webRoot, webIP & webPort.
-*                   And will return the input accordingly after verifying.
+* Description	:	This class holds the definition for the Client Request.
 */
 
 
 using System;
 
+
+/* Name     : Program
+* Purpose   : The purpose of the ClientRequest is to hold the request message and parse its contents.
+*             The class has vitrifaction members to validate the request's contents.
+*/
 public class ClientRequest
 {
-    public string Request { get; private set; }
-    public string[] RequestSplit { get; private set; }
-    public string RequestType { get; private set; }
-    public string Resource { get; private set; }
-    public string ResourceExtension { get; private set; }
-    public string HttpSpecification { get; private set; }
-    public string HostLabel { get; private set; }
-    public string Host { get; private set; }
+    // properties for the request
+    public string Request { get; private set; } // entire request string
+    public string[] RequestSplit { get; private set; } // array for message
+    public string RequestType { get; private set; } // GET or POST
+    public string Resource { get; private set; } // the resource requested e.g. "test.html"
+    public string ResourceExtension { get; private set; } // the extension of the resource e.g. "html"
+    public string HttpSpecification { get; private set; } // the HTTP specification
+    public string HostLabel { get; private set; } // the label "HOST"
+    public string Host { get; private set; } // the host IP
 
+    /*  -- Method Header Comment
+	    * Name	    :	ClientRequest -- CONSTRUCTOR
+	    * Purpose   :	It takes in the client request string and parses it into the properties of the class.
+	    * Inputs	:	inData        -   string
+	    * Outputs	:	NONE
+	    * Returns	:	Nothing
+        */
     public ClientRequest(string inData)
 	{
+        // parse the input data
         Request = inData;
-        RequestSplit = Request.Split(new string[] { "\r\n", " " }, StringSplitOptions.None);
+        RequestSplit = Request.Split(new string[] { "\r\n", " " }, StringSplitOptions.None); // split based on new line and spaces
         RequestType = RequestSplit[0];
         Resource = RequestSplit[1];
 
+        // check for "." in the Resource
         if (Resource.Contains("."))
         {
+            // get the extension
             ResourceExtension = Resource.Substring(Resource.LastIndexOf(".") + 1);
         }
 
+        // parse the remaining input data
         HttpSpecification = RequestSplit[2];
         HostLabel = RequestSplit[3];
         Host = RequestSplit[4];
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyHttpSpecification
+	    * Purpose   :	This method confirms that the request is in specification.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyHttpSpecification()
     {
         bool isValid = false;
-        if (HttpSpecification == "HTTP/1.1")
+        if (HttpSpecification == "HTTP/1.1" && HostLabel == "Host:")
         {
             isValid = true;
         }
         return isValid;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyRequest
+	    * Purpose   :	This method verifies that the server is receiving a post method.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyRequest()
     {
         bool result;
@@ -63,14 +90,25 @@ public class ClientRequest
         return result;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyEndLine
+	    * Purpose   :	This method verifies that the servers receives a "required blank line"
+	    *               at the end of the request.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyEndLine()
     {
         bool isValid = false;
+
+        // make sure that the user doesn't send a message that is clearly invalid
         if (RequestSplit.Length > 2)
         {
+            // get second last and last split strings
             string secondLast = RequestSplit[RequestSplit.Length - 2];
             string last = RequestSplit[RequestSplit.Length - 1];
 
+            // verify that the "required blank line" exists
             if (secondLast == "" && last == "")
             {
                 isValid = true;
@@ -79,9 +117,18 @@ public class ClientRequest
         return isValid;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyResourceExtensionHtmlFiles
+	    * Purpose   :	This method verifies that the resource is an HTML file.
+	    *               at the end of the request.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyResourceExtensionHtmlFiles()
     {
         bool isValid = false;
+
+        // HTML files includes the following extensions
         if (ResourceExtension == "html" || ResourceExtension == "htm" || ResourceExtension == "htmls" || ResourceExtension == "htx")
         {
             isValid = true;
@@ -89,6 +136,12 @@ public class ClientRequest
         return isValid;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyResourceExtensionHttFile
+	    * Purpose   :	This method that the resource is an HTT file (family of HTML).
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyResourceExtensionHttFile()
     {
         bool isValid = false;
@@ -99,6 +152,13 @@ public class ClientRequest
         return isValid;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyResourceExtensionTxtFiles
+	    * Purpose   :	This method verifies that the resource is an TXT file.
+	    *               at the end of the request.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyResourceExtensionTxtFiles()
     {
         bool isValid = false;
@@ -109,6 +169,13 @@ public class ClientRequest
         return isValid;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyResourceExtensionJpgImages
+	    * Purpose   :	This method verifies that the resource is one of the JPG family.
+	    *               at the end of the request.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyResourceExtensionJpgImages()
     {
         bool isValid = false;
@@ -119,6 +186,13 @@ public class ClientRequest
         return isValid;
     }
 
+    /*  -- Method Header Comment
+	    * Name	    :	VerifyResourceExtensionGif
+	    * Purpose   :	This method verifies that the resource is an GIF file.
+	    *               at the end of the request.
+	    * Inputs	:	NONE
+	    * Returns	:	Bool - true if valid otherwise false
+        */
     public bool VerifyResourceExtensionGif()
     {
         bool isValid = false;
