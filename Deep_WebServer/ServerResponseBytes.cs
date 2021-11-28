60b827b99599d87e79a687dfa1d7a70e683c956d
dpatel5451 <dpatel5451@conestogac.on.ca>
2021-11-27 22:31:27 -0500

    Server reponse bytes commentign done

--- Deep_WebServer/ServerResponseBytes.cs
+++ Deep_WebServer/ServerResponseBytes.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+* Filename		:	ServerResponseBytes.cs
+* Project		:	PROG2001 - Assignment 06 
+* Programmer	:	Deep Patel & Sean Mccarthy
+* First Version	:	24/11/2021
+* Description	:	It will store full path and check IP. It will also 
+*                   It will also reads all the contents of the file and 
+*                   it will also get the file information length and also 
+*                   logs Response into the log file.
+*/
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,18 +21,47 @@ using System.Threading.Tasks;
 
 namespace myOwnWebServer
 {
+
+    /* Name      : ServerResponseBytes
+    * Purpose    : The purpose of this class is to set up an server. It gives user an awesome experience of real server.
+    */
     class ServerResponseBytes
     {
+
+        //Gets and sets string FilePath.
         public string FilePath { get; private set; }
+
+        //Gets and sets string Response.
         public string Response { get; private set; }
+
+        //Gets and sets bytes array of FileInformation.
         public byte[] FileInformation { get; private set; }
+
+        //Gets and sets string ContentLength.
         public string ContentLength { get; private set; }
+
+        //Gets and sets string Ip.
         public string Ip { get; private set; }
 
+
+
+        /*  -- Method Header Comment
+	    * Name	    :	ServerResponseBytes -- CONSTRUCTOR
+	    * Purpose   :	It will initializes all the members of ServerResponseBytes class.
+	    *               It will also store the whole file path of a resource.
+	    * Inputs	:	root            -   string
+	    *               fileREsource    -   string
+	    *               ip              -   string
+	    * Outputs	:	NONE
+	    * Returns	:	Nothing
+        */
         public ServerResponseBytes(string root, string fileResource, string ip)
         {
+
+            //Stores the whole file path into filePath.
             FilePath = root + fileResource;
 
+            //Stores ip into local varaible.
             Ip = ip;
 
             //Reads the whole file and will store it into 'fileInformation' string.
@@ -26,8 +69,19 @@ namespace myOwnWebServer
 
             //Stores fileInformation length into 'contentLength' string.
             ContentLength = FileInformation.Length.ToString();
+
         }
 
+
+
+
+        /*  -- Method Header Comment
+            Name	:	GenerateServerResponseJpg 
+            Purpose :	The purpose of this method is to return header data for jpg files.
+            Inputs	:	NONE
+            Returns	:	string          -       Header data for jpg files which will have
+                                                Content type, Content Length, Server and Date.
+        */
         public string GenerateServerResponseJpg()
         {
             //Current date and time.
@@ -39,6 +93,17 @@ namespace myOwnWebServer
             return "HTTP/1.1\r\nContent-Type: image/jpeg\r\nContent-Length: " + ContentLength + "\r\nServer: " + Ip + "\r\nDate: " + time.ToString() + "\r\n\r\n";
         }
 
+
+
+
+
+        /*  -- Method Header Comment
+            Name	:	GenerateServerResponseGif 
+            Purpose :	The purpose of this method is to return header data for gif files.
+            Inputs	:	NONE
+            Returns	:	string          -       Header data for gif files which will have
+                                                Content type, Content Length, Server and Date.
+        */
         public string GenerateServerResponseGif()
         {
             //Current date and time.
