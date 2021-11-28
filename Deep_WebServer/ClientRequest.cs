c4ec02186566361e225f85cea923e4e07926436b
mccarski <48197727+mccarski@users.noreply.github.com>
2021-11-27 22:14:22 -0500

    client request rename

--- Deep_WebServer/ClientRequest.cs
+++ Deep_WebServer/ClientRequest.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+* Filename		:	ServerMessage.cs
+* Project		:	PROG2001 - Assignment 06 
+* Programmer	:	Deep Patel & Sean Mccarthy
+* First Version	:	24/11/2021
+* Description	:	It will verify that user input matches the requirements. 
+*                   It will verify user entered webRoot, webIP & webPort.
+*                   And will return the input accordingly after verifying.
+*/
+
+
+using System;
 
 public class ClientRequest
 {
